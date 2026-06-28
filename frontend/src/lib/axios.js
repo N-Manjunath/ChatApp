aaf161462b9e3199c859ebec68a02fa9d501db60
N-Manjunath <manjunathmanju200727@gmail.com>
2026-06-28 16:10:10 +0530

    axios file fixation

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-const backendUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:5001";
+const backendUrl =
+  import.meta.env.VITE_BACKEND_URL || "http://localhost:5001";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? `${backendUrl}/api` : "/api",
+  baseURL: `${backendUrl}/api`,
   withCredentials: true,
 });
